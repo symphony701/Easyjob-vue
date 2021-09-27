3e0e2712bd0bddcc96bfe6bbaf147988a8a92450
SergioMG1259 <89159015+SergioMG1259@users.noreply.github.com>
2021-09-27 11:23:53 -0500

    an2

--- src/views/Practitioners.vue
+++ src/views/Practitioners.vue
@@ -4,10 +4,11 @@
       <v-col v-for="announce in announcementPr" :key="announcementPr.id">
         <v-card class="mx-auto my-12"
                 min-width="700"
-                min-height="250"
+                min-height="150"
                 max-width="900">
-          <v-card-text>{{announce.especialty}}</v-card-text>
-
+          <v-card-title>{{announce.companyName}}</v-card-title>
+          <v-card-title>{{announce.titleAnnouncement}}</v-card-title>
+          <v-card-subtitle>{{announce.especialty+" | "+announce.experience}}</v-card-subtitle>
         </v-card>
       </v-col>
     </div>
